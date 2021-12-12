0e9b919f041df049207741a0bed1d03abf761836
Jérôme <jerome.cognet@aramisauto.com>
2021-12-12 15:58:27 +0100

    feat: add video

--- back/bin/commandOvh.php
+++ back/bin/commandOvh.php
@@ -4,7 +4,7 @@ $phpPath = 'php';
 $cmd = sprintf('%s %s/console a:n:h -vvv', $phpPath, __DIR__);
 $now = new \DateTime();
 echo $cmd . "\n";
-$output = array();
+$output = [];
 echo $now->format('d/m/Y - H:i:s') . "\n";
 exec($cmd, $output);
 echo implode("\n", $output);
---
+++ back/src/ValueObject/Article/Video.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace App\ValueObject\Article;
+
+use App\ValueObject\BlockInterface;
+use Carbon\Carbon;
+
+class Video implements BlockInterface
+{
+    private const BLOCK_INTERFACE_TYPE = 'video';
+
+    private string $title;
+    private string $body;
+    private Carbon $addedAt;
+    /**
+     * @var Sujet[]
+     */
+    private array $sujets;
+
+    public function __construct(
+        string $title,
+        string $body,
+        Carbon $addedAt,
+        array $sujets
+    ) {
+        $this->title = $title;
+        $this->body = $body;
+        $this->addedAt = $addedAt;
+        $this->sujets = $sujets;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getContent(): string
+    {
+        return $this->body;
+    }
+
+    public function getAddedAt(): Carbon
+    {
+        return $this->addedAt;
+    }
+
+    public function getType(): string
+    {
+        return self::BLOCK_INTERFACE_TYPE;
+    }
+
+    public function getSujets(): array
+    {
+        return $this->sujets;
+    }
+}
