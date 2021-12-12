2da8007b9c61653664a83c03798b6e0114358f21
Jérôme <jerome.cognet@aramisauto.com>
2021-12-12 11:41:46 +0100

    feat: add php command for OVH

---
+++ back/bin/commandOvh.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+$phpPath = 'php';
+$cmd = sprintf('%s %s/console a:n:h -vvv', $phpPath, __DIR__);
+$now = new \DateTime();
+echo $cmd . "\n";
+$output = array();
+echo $now->format('d/m/Y - H:i:s') . "\n";
+exec($cmd, $output);
+echo implode("\n", $output);
