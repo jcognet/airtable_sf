f900130227eb355df68b0a47a62304ef9c46e2f4
Jérôme <jerome.cognet@aramisauto.com>
2021-12-14 16:51:52 +0100

    feat: add layout biere

--- back/src/Service/Color.php
+++ back/src/Service/Color.php
@@ -8,5 +8,6 @@ class Color
     public const PURPLE = '7354c4';
     public const DARK_RED = 'c45461';
 
-    public const PURPLE_SECONDARY = 'E6DDFB';
+    public const PURPLE_SECONDARY = 'e6ddfb';
+    public const DARK_RED_SECONDARY = 'fad9d9';
 }
