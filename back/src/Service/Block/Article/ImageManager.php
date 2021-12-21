5098a1190d8950f10cb15ccd8b397cbcb0185744
Jérôme <jerome.cognet@aramisauto.com>
2021-12-21 15:29:03 +0100

    feat: add image block

---
+++ back/src/Service/Block/Article/ImageManager.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\Block\Article;
+
+use App\Service\AirTable\Article\ImageClient;
+use App\Service\Block\BlockManagerInterface;
+use App\ValueObject\BlockInterface;
+
+class ImageManager implements BlockManagerInterface
+{
+    private ImageClient $imageClient;
+
+    public function __construct(ImageClient $imageClient)
+    {
+        $this->imageClient = $imageClient;
+    }
+
+    public function getContent(): BlockInterface
+    {
+        return $this->imageClient->fetchRandomData();
+    }
+}
