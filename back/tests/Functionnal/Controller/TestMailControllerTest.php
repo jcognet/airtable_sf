cc6ee6c494954f8e80199069ffae47f5d75f1517
Jérôme <jerome.cognet@aramisauto.com>
2022-01-03 14:23:07 +0100

    feat: add test

---
+++ back/tests/Functionnal/Controller/TestMailControllerTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Functionnal\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * @internal
+ */
+final class TestMailControllerTest extends WebTestCase
+{
+    public function test_default(): void
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
+        $client->setServerParameter('HTTP_HOST', 'localhost:7000');
+        $client->followRedirects(true);
+
+        // Request a specific page
+        $crawler = $client->request('GET', '/test/mail/show/?date=2022-01-03');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_sunday(): void
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
+        $client->setServerParameter('HTTP_HOST', 'localhost:7000');
+        $client->followRedirects(true);
+
+        // Request a specific page
+        $crawler = $client->request('GET', '/test/mail/show/?date=2022-01-02');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_saturday(): void
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
+        $client->setServerParameter('HTTP_HOST', 'localhost:7000');
+        $client->followRedirects(true);
+
+        // Request a specific page
+        $crawler = $client->request('GET', '/test/mail/show/?date=2022-01-02');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+    }
+}
