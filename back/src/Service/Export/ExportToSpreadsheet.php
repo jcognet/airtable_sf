a4365587c648b93290e2eeff39bd3b48f912098a
Jérôme <jerome.cognet@aramisauto.com>
2022-01-09 15:46:20 +0100

    feat: add image number in export

--- back/src/Service/Export/ExportToSpreadsheet.php
+++ back/src/Service/Export/ExportToSpreadsheet.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Service\Export;
 
 use App\Service\AirTable\Article\ALireClient;
+use App\Service\AirTable\Article\ImageClient;
 use App\Service\AirTable\Article\LuClient;
 use App\Service\AirTable\Book\BookClient;
 use App\Service\AirTable\ToDo\ItemClient;
@@ -17,19 +18,22 @@ class ExportToSpreadsheet
     private ItemClient $itemClient;
     private ALireClient $ALireClient;
     private BookClient $bookClient;
+    private ImageClient $imageClient;
 
     public function __construct(
         ExportAirtableWriter $exportAirtableWriter,
         LuClient $luClient,
         ItemClient $itemClient,
         ALireClient $ALireClient,
-        BookClient $bookClient
+        BookClient $bookClient,
+        ImageClient $imageClient
     ) {
         $this->exportAirtableWriter = $exportAirtableWriter;
         $this->luClient = $luClient;
         $this->itemClient = $itemClient;
         $this->ALireClient = $ALireClient;
         $this->bookClient = $bookClient;
+        $this->imageClient = $imageClient;
     }
 
     public function export(): int
@@ -43,6 +47,7 @@ class ExportToSpreadsheet
                     count($this->ALireClient->findAll()),
                     count($this->bookClient->findAll(['filterByFormula' => '{Status} = "A lire"'])),
                     count($this->bookClient->findAll(['filterByFormula' => '{Status} = "Fini"'])),
+                    count($this->imageClient->findAll()),
                 ],
             ]
         );
