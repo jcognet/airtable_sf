4305b2ec2a89d9aee251dfc5548922188c38a56d
Jérôme <jerome.cognet@aramisauto.com>
2022-08-27 20:59:04 +0200

    fix: add new status

--- back/src/ValueObject/Article/Status.php
+++ back/src/ValueObject/Article/Status.php
@@ -7,13 +7,16 @@ class Status
 {
     public const IN_PROGRESS = 'in_progress';
     public const TO_DO = 'to_do';
+    public const SUMMARY_TO_WRITE = 'summary_to_write';
 
     public const AT_IN_PROGRESS = 'In progress';
     public const AT_TO_DO = 'Todo';
+    public const AT_SUMMARY_TO_WRITE = 'Summary to write';
 
     private const CONVERT_FROM_AIRTABLE = [
         self::AT_IN_PROGRESS => self::IN_PROGRESS,
         self::AT_TO_DO => self::TO_DO,
+        self::AT_SUMMARY_TO_WRITE => self::SUMMARY_TO_WRITE,
     ];
 
     public string $value;
