7c9539fe37e3bdeaf6e3c95cd10cf6ff4d57534b
Jérôme <jerome.cognet@aramisauto.com>
2022-09-29 20:21:23 +0200

    fix: add return line if cli

---
+++ back/src/Extension/NavigationExtension.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Extension;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class NavigationExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('is_google_mail', [$this, 'isGoogleMail']),
+        ];
+    }
+
+    public function isGoogleMail(): bool
+    {
+        return PHP_SAPI === 'cli';
+    }
+}
