587733769b250cec5f4612b8653eb9837c29cb7d
Jérôme <jcognet@gmail.com>
2023-03-31 11:30:52 +0200

    feat: feat add dashboard

---
+++ back/src/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route(path: '/', name: 'index', methods: ['GET'])]
+    public function index(
+        Request $request
+    ): Response {
+        return $this->redirectToRoute('dashboard_show', $request->query->all());
+    }
+}
---
+++ back/tests/Functionnal/DashboardControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Functionnal\Controller\Newspaper;
+
+use App\Tests\Functionnal\SetUserTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * @internal
+ */
+final class DashboardControllerTest extends WebTestCase
+{
+    use SetUserTrait;
+
+    public function test_default(): void
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
+        $client->followRedirects(true);
+        $this->loginUser($client);
+
+        // Request a specific page
+        $client->request('GET', '/dashboard');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function test_example(): void
+    {
+        // This calls KernelTestCase::bootKernel(), and creates a
+        // "client" that is acting as the browser
+        $client = static::createClient();
+        $client->followRedirects(true);
+        $this->loginUser($client);
+
+        // Request a specific page
+        $client->request('GET', '/example');
+
+        // Validate a successful response and some content
+        $this->assertResponseIsSuccessful();
+    }
+}
