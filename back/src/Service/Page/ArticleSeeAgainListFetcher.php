a0a68f4f669b2641111113d0c1efe3c845de6a3f
Jérôme <jcognet@gmail.com>
2023-04-15 15:52:05 +0200

    feat: add see again block in dashboard

---
+++ back/src/Service/Page/ArticleSeeAgainListFetcher.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\Page;
+
+use App\Service\Block\BlockFinder;
+use App\ValueObject\Article\ArticleSeeAgainList;
+use App\ValueObject\NewsletterBlockManager\BlockType;
+use App\ValueObject\Newspaper;
+
+class ArticleSeeAgainListFetcher
+{
+    public function __construct(
+        private readonly BlockFinder $blockFinder
+    ) {
+    }
+
+    public function fetch(Newspaper $newspaper): ?ArticleSeeAgainList
+    {
+        $blocks = $this->blockFinder->findInNewspaper(
+            $newspaper,
+            BlockType::SEE_AGAIN
+        );
+
+        if ($blocks !== null) {
+            return $blocks[0];
+        }
+
+        return null;
+    }
+}
