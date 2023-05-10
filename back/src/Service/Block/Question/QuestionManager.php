2fcc67594d084b8217650523fd7459a06a57063c
Jérôme <jcognet@gmail.com>
2023-05-10 11:10:55 +0200

    fix: use question lister instead of querying AT

--- back/src/Service/Block/Question/QuestionManager.php
+++ back/src/Service/Block/Question/QuestionManager.php
@@ -3,18 +3,20 @@ declare(strict_types=1);
 
 namespace App\Service\Block\Question;
 
-use App\Service\AirTable\Qcm\QuestionsClient;
 use App\Service\Block\BlockManagerInterface;
+use App\Service\Import\Airtable\Qcm\Question\QuestionLister;
 use App\ValueObject\BlockInterface;
 
 class QuestionManager implements BlockManagerInterface
 {
-    public function __construct(private readonly QuestionsClient $questionsClient)
+    public function __construct(private readonly QuestionLister $questionLister)
     {
     }
 
     public function getContent(): ?BlockInterface
     {
-        return $this->questionsClient->fetchRandomData();
+        $list = $this->questionLister->list();
+
+        return $list[array_rand($list)];
     }
 }
