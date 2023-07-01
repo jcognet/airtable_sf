c88ee656bfa524fbce51b81cbacadd7e363497c8
Jérôme <jcognet@gmail.com>
2023-07-01 16:51:12 +0200

    fix: correct URL article builder

--- back/src/Service/Builder/Article/AbstractArticleBuilder.php
+++ back/src/Service/Builder/Article/AbstractArticleBuilder.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Service\Builder\Article;
 
+use App\Exception\Builder\UnknownArticleBuilderException;
 use App\Service\AirTable\UrlBuilder;
 use App\Service\Builder\BuilderInterface;
 use App\Service\Repository\Article\SujetRepository;
@@ -13,7 +14,8 @@ use Carbon\Carbon;
 
 abstract class AbstractArticleBuilder implements BuilderInterface
 {
-    private const TABLE_URL = 'tblPLpYPyAOT2q13Q';
+    private const TABLE_URL_LU = 'tblPLpYPyAOT2q13Q';
+    private const TABLE_URL_TO_READ = 'tbla72IEwy6EYrX2I';
 
     public function __construct(
         private readonly SujetRepository $sujetRepository,
@@ -45,11 +47,32 @@ abstract class AbstractArticleBuilder implements BuilderInterface
             $status,
             $data['fields']['URL'] ?? '',
             isset($data['fields']['Type']) ? new ArticleType($data['fields']['Type']) : null,
-            $this->urlBuilder->build($this->airtableAppArticleId, self::TABLE_URL, $this->getViewUrl(), $data['id']),
+            $this->urlBuilder->build($this->airtableAppArticleId, $this->getTableURL(), $this->getViewUrl(), $data['id']),
             $data['fields']['Conceptualisé'] ?? false,
             $data['fields']['A revoir'] ?? false
         );
     }
 
     abstract public function getViewUrl(): string;
+
+    private function getTableURL(): string
+    {
+        switch (static::class) {
+            case ArticleToReadBuilder::class:
+                return self::TABLE_URL_TO_READ;
+            case ArticleReadBuilder::class:
+                return self::TABLE_URL_LU;
+        }
+
+        throw new UnknownArticleBuilderException(
+            sprintf(
+                'Unknown class: %s, available are: %s',
+                static::class,
+                implode(
+                    ', ',
+                    [ArticleToReadBuilder::class, ArticleReadBuilder::class]
+                )
+            )
+        );
+    }
 }
