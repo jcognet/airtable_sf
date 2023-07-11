d690846bb9cd060c42a14b5bf3548f236d3fbbf6
Jérôme <jcognet@gmail.com>
2023-07-11 14:20:35 +0200

    feat: add download controller & update file listing

---
+++ back/src/Controller/DownloadController.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Service\Picture\EncoderDecoder;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Mime\MimeTypes;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DownloadController extends AbstractController
+{
+    #[Route(path: '/file/download/${pathFile}', name: 'file_download', methods: ['GET'])]
+    public function list(
+        EncoderDecoder $encoderDecoder,
+        string $pathFile
+    ): Response {
+        $file = $encoderDecoder->decode($pathFile);
+
+        if (!is_file($file)) {
+            throw $this->createNotFoundException(sprintf('Unknown file: %s', $file));
+        }
+
+        $mimeTypes = new MimeTypes();
+
+        return new BinaryFileResponse(
+            $file,
+            headers: ['Content-Type' => $mimeTypes->getMimeTypes($file)],
+            contentDisposition: ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+        );
+    }
+}
