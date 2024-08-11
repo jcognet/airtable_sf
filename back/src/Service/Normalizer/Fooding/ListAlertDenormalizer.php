13c33cfe7f9c492f3a69b0606d6ed9f906ce4b20
jcognet <jcognet@ext.ccmbenchmark.fr>
2024-08-11 14:45:41 +0200

    fix: list alert denormalization

---
+++ back/src/Service/Normalizer/Fooding/ListAlertDenormalizer.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\Normalizer\Fooding;
+
+use App\Service\Alert\ListAlertBlock;
+use App\ValueObject\Alert\ListAlert;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+class ListAlertDenormalizer implements DenormalizerInterface
+{
+    public function denormalize(mixed $data, string $type, string $format = null, array $context = []): ListAlertBlock
+    {
+        $data['listAlert'] = new ListAlert($data['content']);
+        unset($data['title'], $data['content'], $data['class']);
+
+        return new ListAlertBlock(...$data);
+    }
+
+    public function supportsDenormalization(mixed $data, string $type, string $format = null, array $context = []): bool
+    {
+        return $type === ListAlertBlock::class;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return ['*' => true];
+    }
+}
