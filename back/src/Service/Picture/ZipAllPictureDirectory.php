736b3b9cb0917197e32d5f90d75827c2a9d1e15d
jcognet <jcognet@ext.ccmbenchmark.fr>
2025-01-20 16:09:54 +0100

    feat: correct timeout zip

--- back/src/Service/Picture/ZipAllPictureDirectory.php
+++ back/src/Service/Picture/ZipAllPictureDirectory.php
@@ -5,10 +5,12 @@ namespace App\Service\Picture;
 
 use App\Exception\Picture\DirectoryNotZippableException;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Finder\SplFileInfo;
 
 class ZipAllPictureDirectory
 {
     private const FILE_NAME = 'files.zip';
+    private const NB_DIRECTORIES_TO_ZIP = 30;
 
     public function __construct(
         private readonly DirectoryLister $directoryLister,
@@ -24,7 +26,7 @@ class ZipAllPictureDirectory
             return;
         }
 
-        foreach ($dirs as $directory) {
+        foreach ($this->filterDirectories($dirs) as $directory) {
             $this->logger->debug(sprintf('Current directory: %s', $directory->getRealPath()));
             $zip = new \ZipArchive();
             $zipFileName = self::getFileName($directory->getRealPath());
@@ -60,5 +62,32 @@ class ZipAllPictureDirectory
             $absolutePath,
             self::FILE_NAME
         );
+    }
+
+    private function filterDirectories(array $directories): array
+    {
+        usort(
+            $directories,
+            static function (SplFileInfo $a, SplFileInfo $b) {
+                $fileA = self::getFileName($a->getRealPath());
+                $fileB = self::getFileName($b->getRealPath());
+
+                if (file_exists($fileA) && file_exists($fileB)) {
+                    return $a->getMTime() <=> $b->getMTime();
+                }
+
+                if (!file_exists($fileA) && !file_exists($fileB)) {
+                    return $a->getMTime() <=> $b->getMTime();
+                }
+
+                if (!file_exists($fileA)) {
+                    return -1;
+                }
+
+                return 1;
+            }
+        );
+
+        return array_slice($directories, 0, self::NB_DIRECTORIES_TO_ZIP);
     }
 }
