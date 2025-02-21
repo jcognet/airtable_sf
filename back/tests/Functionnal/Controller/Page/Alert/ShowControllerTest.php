203ceb8a8c3193d18b54ca1f3318753ff2b2f94f
jcognet <jcognet@ext.ccmbenchmark.fr>
2025-02-21 11:59:01 +0100

    fix: carbon value in alert

--- back/tests/Functionnal/Controller/Page/Alert/ShowControllerTest.php
+++ back/tests/Functionnal/Controller/Page/Alert/ShowControllerTest.php
@@ -20,9 +20,16 @@ final class ShowControllerTest extends WebTestCase
         $client->followRedirects(true);
         $this->loginUser($client);
 
-        $client->request(Request::METHOD_GET, '/alert/');
+        $client->request(Request::METHOD_GET, '/alert/?date=2025-02-21');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1.test-alert', 'alertes');
+        $this->assertSelectorTextContains('#test-alert-abs_max_series', '16');
+        $this->assertSelectorTextContains('#test-alert-meat_current_without_series', '0');
+        $this->assertSelectorTextContains('#test-alert-abs_current_series', '3');
+        $this->assertSelectorTextContains('#test-alert-abs', '6');
+        $this->assertSelectorTextContains('#test-alert-cut', '4');
+        $this->assertSelectorTextContains('#test-alert-qi', '1');
+        $this->assertSelectorTextContains('#test-alert-coloring', '15');
     }
 }
